9f3224e88323c732e171f11ace7ddf6e4de9c181
yong geon <gunnylove@gmail.com>
2025-09-07 17:23:53 +0900

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // 서버 빌드 시 dummy-data 폴더를 포함하도록 설정
+      config.module.rules.push({
+        test: /\.json$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'dummy-data/[name][ext]'
+        }
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
